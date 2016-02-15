2d183c29e76e047c3596eb20f26c983ab43f0b45
Rafael F.V.C. Santos <rafaelfvcs@gmail.com>
2016-02-15 20:48:08 -0300

    Add status message class

---
+++ src/main/java/com/nfracgen/webmanager/view/MessagesView.java
@@ -0,0 +1,25 @@
+package com.nfracgen.webmanager.view;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+ 
+@ManagedBean
+public class MessagesView {
+     
+    public void info() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Dados salvo com sucesso!"));
+    }
+     
+    public void warn() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Warning!", "Watch out for NFracGen."));
+    }
+     
+    public void error() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "Contact admin."));
+    }
+     
+    public void fatal() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Fatal!", "System Error"));
+    }
+}
